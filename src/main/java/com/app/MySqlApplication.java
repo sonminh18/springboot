a9699909c35843060653f2f22abb6cc8acbca135
Anushka <anushka@devtron.ai>
2020-05-26 16:55:44 +0530

    change in mapping

--- src/main/java/com/app/MySqlApplication.java
+++ src/main/java/com/app/MySqlApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequestMapping("/my-java-app")
+@RequestMapping("/")
 public class MySqlApplication {
 
 	public static void main(String[] args) {
