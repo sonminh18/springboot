a92794096d1071836ca0b2c6939db0fda127d986
Anushka <anushka@devtron.ai>
2020-05-26 18:13:28 +0530

    change in mapping

--- src/main/java/com/app/MySqlApplication.java
+++ src/main/java/com/app/MySqlApplication.java
@@ -2,12 +2,12 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@RestController
+@ComponentScan({"com.app.controllers"})
+
 public class MySqlApplication {
-Logger logger = LoggerFactory.getLogger(MySqlApplication.class);
-	@GetMapping("/")
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySqlApplication.class, args);
