3aa920dfd5756c3b21c0c0a5acaa2b6ef210d5e2
Anushka <anushka@devtron.ai>
2020-05-26 18:31:58 +0530

    change in mapping

--- src/main/java/com/app/MySqlApplication.java
+++ src/main/java/com/app/MySqlApplication.java
@@ -2,10 +2,8 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.app.controllers"})
 
 public class MySqlApplication {
 
